0b57bcc23f802c633d1c67994b4b72d9b47f0452
Kerollos <kerolloz@yahoo.com>
2019-12-14 21:32:05 +0200

    Fix refactoring mistakes

--- KNN.m
+++ KNN.m
@@ -3,8 +3,8 @@ function KNN(n, img_name)
     img = imread(img_name);
     class(img)
     imshow(img);
-    [r, c, s] = size(img);
-    sample_region = false([r, c, n]);
+    [rows_, columns_, ~] = size(img);
+    sample_region = false([rows_, columns_, n]);
     f = figure;
 
     for i = 1:n
@@ -17,15 +17,14 @@ function KNN(n, img_name)
     for i = 1:n
         figure
         imshow(sample_region(:, :, i));
-        title(['sample region' num2str(i)])
+        title(['sample region ' num2str(i)])
     end
 
     r = img(:, :, 1);
     g = img(:, :, 2);
     b = img(:, :, 3);
 
-    color_marker = repmat(0, [n, 3]);
-    colorss = repmat(0, [n, 3]);
+    color_marker = zeros([n, 3]);
 
     for i = 1:n
         color_marker(i, 1) = mean2(r(sample_region(:, :, i)));
@@ -34,28 +33,28 @@ function KNN(n, img_name)
     end
 
     colorss = color_marker;
-    color_lables = 0:n - 1;
+    color_lables = 1:n;
 
     r = double(r);
     g = double(g);
     b = double(b);
 
-    distance = repmat(0, [size(g), n]);
+    distance = zeros([size(g), n]);
 
     for i = 1:n
-        distance(:, :, i) = (((g - color_marker(i, 2)).^2) + ((b - color_marker(i, 3)).^2) + ((r - color_marker(i, 1)).^2)).^0.5;
+        distance(:, :, i) = (((r - color_marker(i, 1)).^2) + ((g - color_marker(i, 2)).^2) + ((b - color_marker(i, 3)).^2)).^0.5;
     end
 
-    [value, labels] = min(distance, [], 3);
+    [~, labels] = min(distance, [], 3);
     labels = color_lables(labels);
     y = zeros(size(img));
-    ll = double(labels) + 1;
+    ll = double(labels);
     colorss = int16(colorss);
 
-    for m = 1:r
+    for i = 1:rows_
 
-        for n = 1:c
-            y(m, n, :) = colorss(ll(m, n), :);
+        for j = 1:columns_
+            y(i, j, :) = colorss(ll(i, j), :);
         end
 
     end
