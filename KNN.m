4e71db7c56f993ef690aa3bf4c2e6dbaaeadea24
Kerollos <kerolloz@yahoo.com>
2019-12-14 19:36:58 +0200

    code formatting

--- KNN.m
+++ KNN.m
@@ -1,61 +1,67 @@
-function KNN(n, imgname)
-
-    x = imread(imgname);
-    class(x)
-    imshow(x);
-    [r, c, s] = size(x);
+function KNN(n, img_name)
+    % n => neighbours
+    img = imread(img_name);
+    class(img)
+    imshow(img);
+    [r, c, s] = size(img);
     sample_region = false([r, c, n]);
     f = figure;
+
     for i = 1:n
-        set(f, 'name', ['select samole for region ' (i+'0')]);
-        sample_region(:,:,i)=roipoly(x);
+        set(f, 'name', ['select sample for region ' num2str(i)]);
+        sample_region(:, :, i) = roipoly(img);
     end
+
     close(f);
+
     for i = 1:n
         figure
-        imshow(sample_region(:,:,i));
-        title(['sample region' (i+'0')])
+        imshow(sample_region(:, :, i));
+        title(['sample region' num2str(i)])
     end
-    
-    
-    l=x(:,:,1);
-    a=x(:,:,2);
-    b=x(:,:,3);
-    
-    color_marker=repmat(0,[n,3]);
-    colorss=repmat(0,[n,3]);
+
+    r = img(:, :, 1);
+    g = img(:, :, 2);
+    b = img(:, :, 3);
+
+    color_marker = repmat(0, [n, 3]);
+    colorss = repmat(0, [n, 3]);
+
     for i = 1:n
-        color_marker(i,2)=mean2(a(sample_region(:,:,i)));
-        color_marker(i,3)=mean2(b(sample_region(:,:,i)));
-        color_marker(i,1)=mean2(l(sample_region(:,:,i)));
-        
-        
+        color_marker(i, 1) = mean2(r(sample_region(:, :, i)));
+        color_marker(i, 2) = mean2(g(sample_region(:, :, i)));
+        color_marker(i, 3) = mean2(b(sample_region(:, :, i)));
     end
-    colorss=color_marker;
-    color_lables = 0:n-1;
-    a=double(a);
-    b=double(b);
-    l=double(l);
-    distance = repmat(0,[size(a),n]);
+
+    colorss = color_marker;
+    color_lables = 0:n - 1;
+
+    r = double(r);
+    g = double(g);
+    b = double(b);
+
+    distance = repmat(0, [size(g), n]);
+
     for i = 1:n
-        distance(:,:,i)=(((a-color_marker(i,2)).^2)+((b-color_marker(i,3)).^2)+((l-color_marker(i,1)).^2)).^0.5;
-        
+        distance(:, :, i) = (((g - color_marker(i, 2)).^2) + ((b - color_marker(i, 3)).^2) + ((r - color_marker(i, 1)).^2)).^0.5;
     end
-    [value,labels]=min(distance,[],3);
-    labels=color_lables(labels);
-    y=zeros(size(x));
-    ll=double(labels)+1;
+
+    [value, labels] = min(distance, [], 3);
+    labels = color_lables(labels);
+    y = zeros(size(img));
+    ll = double(labels) + 1;
     colorss = int16(colorss);
-   
-    
+
     for m = 1:r
+
         for n = 1:c
-            y(m,n,:)=colorss(ll(m,n),:);
+            y(m, n, :) = colorss(ll(m, n), :);
         end
+
     end
-    y=uint8(y);
+
+    y = uint8(y);
     figure; imshow(y)
     colorbar
-   
-    
+
 end
--- K_means.m
+++ K_means.m
@@ -1,36 +1,38 @@
 function K_means(k, imgname)
-    
-RGB = imread(imgname);
-figure
-imshow(RGB)
-[L,Centers] = imsegkmeans(RGB,k);
-B = labeloverlay(RGB,L);
-figure
-imshow(B)
-title('Labeled Image')
-title('Labeled Image')
-wavelength = 2.^(0:5) * 3;
-orientation = 0:45:135;
-g = gabor(wavelength,orientation);
-I = rgb2gray(im2single(RGB));
-gabormag = imgaborfilt(I,g);
-figure
-montage(gabormag,'Size',[4 6])
-for i = 1:length(g)
-    sigma = 0.5*g(i).Wavelength;
-    gabormag(:,:,i) = imgaussfilt(gabormag(:,:,i),3*sigma); 
-end
-figure
-montage(gabormag,'Size',[4 6])
-nrows = size(RGB,1);
-ncols = size(RGB,2);
-[X,Y] = meshgrid(1:ncols,1:nrows);
-featureSet = cat(3,I,gabormag,X,Y);
-size(RGB)
-size(featureSet)
-L2 = imsegkmeans(featureSet,k,'NormalizeInput',true);
-C = labeloverlay(RGB,L2);
-figure
-imshow(C)
-title('Labeled Image with Additional Pixel Information')
+
+    RGB = imread(imgname);
+    figure
+    imshow(RGB)
+    [L, Centers] = imsegkmeans(RGB, k);
+    B = labeloverlay(RGB, L);
+    figure
+    imshow(B)
+    title('Labeled Image')
+    title('Labeled Image')
+    wavelength = 2.^(0:5) * 3;
+    orientation = 0:45:135; % 0, 45, 90, 135
+    g = gabor(wavelength, orientation);
+    I = rgb2gray(im2single(RGB));
+    gabormag = imgaborfilt(I, g);
+    figure
+    montage(gabormag, 'Size', [4 6])
+
+    for i = 1:length(g)
+        sigma = 0.5 * g(i).Wavelength;
+        gabormag(:, :, i) = imgaussfilt(gabormag(:, :, i), 3 * sigma);
+    end
+
+    figure
+    montage(gabormag, 'Size', [4 6])
+    nrows = size(RGB, 1);
+    ncols = size(RGB, 2);
+    [X, Y] = meshgrid(1:ncols, 1:nrows);
+    featureSet = cat(3, I, gabormag, X, Y);
+    size(RGB)
+    size(featureSet)
+    L2 = imsegkmeans(featureSet, k, 'NormalizeInput', true);
+    C = labeloverlay(RGB, L2);
+    figure
+    imshow(C)
+    title('Labeled Image with Additional Pixel Information')
 end
